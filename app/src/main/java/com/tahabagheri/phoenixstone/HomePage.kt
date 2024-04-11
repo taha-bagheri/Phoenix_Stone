019b7adbe2fa3178921f5565b2d5fadf421d8ac9
taha-bagheri <baghery78@gmail.com>
2024-04-11 15:51:52 +0200

    Initial commit

---
+++ app/src/main/java/com/tahabagheri/phoenixstone/HomePage.kt
@@ -0,0 +1,36 @@
+package com.tahabagheri.phoenixstone
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+
+class HomePage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.home_view)
+
+        val imgBtnInformation = findViewById<ImageView>(R.id.img_btn_information)
+        val imgBtnAbout = findViewById<ImageView>(R.id.img_btn_about)
+        val imgBtnContact = findViewById<Button>(R.id.btn_contact)
+
+        imgBtnInformation.setOnClickListener {
+            // Aquí abrimos la actividad InformationPage.kt
+            val intent = Intent(this, InformationPage::class.java)
+            startActivity(intent)
+        }
+
+        imgBtnAbout.setOnClickListener{
+            // Aquí abrimos la actividad AboutPage.kt
+            val intent2 = Intent(this, AboutPage::class.java)
+            startActivity(intent2)
+        }
+
+        imgBtnContact.setOnClickListener{
+            // Aquí abrimos la actividad ContactPage.kt
+            val intent3 = Intent(this, ContactPage::class.java)
+            startActivity(intent3)
+        }
+    }
+}
---
+++ app/src/main/java/com/tahabagheri/phoenixstone/InformationPage.kt
@@ -0,0 +1,31 @@
+package com.tahabagheri.phoenixstone
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+
+class InformationPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.information_view)
+
+        val imgBtnContact = findViewById<Button>(R.id.btn_contact)
+        val logoAppInformation = findViewById<ImageView>(R.id.logoApp_information)
+
+        imgBtnContact.setOnClickListener{
+            // Aquí abrimos la actividad ContactPage.kt
+            val intent3 = Intent(this, ContactPage::class.java)
+            startActivity(intent3)
+        }
+
+        logoAppInformation.setOnClickListener{
+            // Aquí abrimos la actividad ContactPage.kt
+            val intent4 = Intent(this, HomePage::class.java)
+            startActivity(intent4)
+        }
+
+        // TODO: add a function for logoapp,for openin Homepage 
+    }
+}
